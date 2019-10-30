feade8ce9abde96af31bf51c0ac9de5737c7509b
Sandro <sandroseryani@yahoo.com>
2019-10-30 17:02:17 -0400

    Changed calculator.js

--- med-school-calculator/src/apps/Calculator/components/Calculator.js
+++ med-school-calculator/src/apps/Calculator/components/Calculator.js
@@ -1,8 +1,54 @@
 import React from 'react';
+import styled from "styled-components";
+import Chevron from "./Chevron"
+import CalculatorData from "./CalculatorData";
+
+const CalculatorWrapper = styled.div`
+    height: 90%;
+    width: 90%;
+    background-color: #DCDCDC99;
+    margin: auto;
+`  
+
+const NavBar = styled.ul`
+    list-style: none; 
+    overflow: hidden; 
+    margin: auto auto 2vh auto;
+`
+
+const NavWrapper = styled.div`
+    background-color: black;
+    margin: auto;
+
+`
 
 class Calculator extends React.Component {
+
+    // Plan: 
+    // 1. Retrieve all types from database in following function 
+    retrieveTypeNames() {
+        // Here make a call to firebase to get all the type names and store in array containing Name and Id
+        return Number.parseInt(this.props.history.location.pathname.split('/')[2]);
+        // In render(), use a for loop to add Chevron for each type
+    }
+
     render() {
-        return <div>Calculator</div>
+        return (
+
+                <CalculatorWrapper>
+                    <NavWrapper>
+                        <NavBar>
+                            <Chevron link="/calculator/1" title="Application Submission"> </Chevron>
+                            <Chevron link="/calculator/2" title="Interview Process"> </Chevron>
+                            <Chevron link="/calculator/3" title="MCAT"> </Chevron>
+                            <Chevron link="/calculator/4" title="Finish"> </Chevron>
+                        </NavBar>
+                    </NavWrapper>
+                    <CalculatorData stage={this.retrieveTypeNames()}>
+
+                    </CalculatorData>
+                </CalculatorWrapper>
+        );
     }
 }
 
