9ff9d784ff6c35c25fbda3730aa6d2424a71f455
engalex1 <47371471+engalex1@users.noreply.github.com>
2019-10-30 02:51:14 -0400

    info display

--- med-school-calculator/src/apps/Profile/components/Profile.js
+++ med-school-calculator/src/apps/Profile/components/Profile.js
@@ -1,8 +1,53 @@
 import React from 'react';
 
+import { getCurrentUser } from 'utils/currentUser';
+import Button from 'common/Button';
+
+
 class Profile extends React.Component {
+
+    state = {
+        currentUser: getCurrentUser(),
+        page: PROFILEPAGES.default
+    };
+
+    handleSwitchPage = page => {
+        this.setState({ page });
+    };
+
+    renderAccountInfo = () => {
+        const { displayName, email } = this.state.currentUser;
+        return (
+            <>
+                <div>Profile</div>
+                Name: {displayName}
+                Email: {email}
+                <Button
+                    text="Edit Password"
+                    onClick={() =>
+                        this.handleSwitchPage(PROFILEPAGES.changepassword)
+                    }
+                />
+                <Button
+                    text="Edit Profile"
+                    onClick={() => this.handleSwitchPage(PROFILEPAGES.changeinfo)}
+                />
+            </>
+        );
+    };
+
     render() {
-        return <div>Profile</div>
+        return (
+            <>
+            {page === PROFILEPAGES.default ? (
+                    this.renderAccountInfo()
+                ) : page === PROFILEPAGES.changepassword ? (
+                    <ChangePassword handleSwitchPage={this.handleSwitchPage}/>
+                ) : page === PROFILEPAGES.changeinfo ? (
+                    <ChangeInfo handleSwitchPage={this.handleSwitchPage}/>
+                ) : null}
+            </>
+        );
     }
 }
 
