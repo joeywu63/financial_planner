a62b46bb6b1d05dd7fe8c78b3b39fc3a11aa4bb2
Yijia Xu <yijiaxu3@gmail.com>
2019-11-19 09:27:07 -0500

    Add button styles

--- med-school-calculator/src/common/Button.js
+++ med-school-calculator/src/common/Button.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import {COLOURS} from "../utils/constants";
 
 const StyledButton = styled.button`
-    height: 20px;
+    height: 35px;
+    font-size: 16px;
+    font-weight: 500;
+    background-color: ${COLOURS.buttonblue};
+    color: ${COLOURS.white};
+    text-align: center;
+    border-radius: 8px;
+    padding: 0 0.9em;
+    margin: 0.15em;
+    cursor: pointer;
+    :hover {
+        background-color: ${COLOURS.greyblue};
+    }
 `;
 
 class Button extends React.Component {
--- med-school-calculator/src/utils/constants.js
+++ med-school-calculator/src/utils/constants.js
@@ -2,3 +2,9 @@ export const ROLES = {
     user: 0,
     admin: 1
 };
+
+export const COLOURS = {
+    white: '#ffffff',
+    buttonblue: '#308bb6',
+    greyblue: '#7398b1'
+};
