97437899d7161319b38fccd4ce8926c1805dd8b8
engalex1 <47371471+engalex1@users.noreply.github.com>
2019-11-30 05:35:29 -0500

    forgot password popup

---
+++ med-school-calculator/src/apps/Login/components/ForgotPassword.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { auth } from 'firebase';
+import styled from 'styled-components';
+import { COLOURS } from 'utils/constants';
+import Button from 'common/Button';
+import SubmitButton from 'common/SubmitButton';
+import { errorToast } from 'utils/helpers';
+
+const Popup = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const PopupInner = styled.div`
+    position: absolute;  
+    left: 35%;  
+    right: 35%;  
+    top: 35%;  
+    bottom: 35%;  
+    margin: auto;  
+    background: white;  
+    display: flex;
+    flex-direction: column;
+    align-content: center
+    align-items: center
+    justify-content: center
+`;
+
+const StyledButton = styled(Button)`
+    width: 190px;
+    margin-top: 10px;
+`;
+
+const StyledInput = styled.input`
+    margin-top: 10px;
+    margin-bottom: 15px;
+    width: 450px;
+`;
+
+const Title = styled.h1`
+    font-weight: 500;
+    font-size: 24px;
+    color: ${COLOURS.darkgrey};
+    text-align: center;
+    line-height: 1.5em;
+    margin-bottom: 30px;
+    margin-top: 0.2em;
+`;
+
+const CenteredDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-content: center
+    align-items: center
+    justify-content: center
+`;
+
+class ForgotPassword extends React.Component {
+    state = {
+        email: ''
+    };
+
+    handleChange = event => {
+        const key = event.target.getAttribute('name');
+        const value = event.target.value;
+
+        this.setState({
+            [key]: value
+        });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        auth()
+            .sendPasswordResetEmail(this.state.email)
+            .then(() => {
+                window.alert('Please Check Your Email For Password Reset');
+            })
+            .catch(err => {
+                errorToast();
+            });
+    };
+
+    render() {
+        return (
+            <Popup>
+                <PopupInner>
+                    <Title>Account Recovery</Title>
+                    Please Enter the Email Address Associated With Your Account:
+                    <form onSubmit={this.handleSubmit}>
+                        <StyledInput
+                            name="email"
+                            type="text"
+                            placeholder="E-mail"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                        <CenteredDiv>
+                            <SubmitButton value="Send Verification Email" />
+                        </CenteredDiv>
+                        <CenteredDiv>
+                            <StyledButton
+                                onClick={this.props.closePopup}
+                                text="Cancel"
+                            ></StyledButton>
+                        </CenteredDiv>
+                    </form>
+                </PopupInner>
+            </Popup>
+        );
+    }
+}
+
+export default ForgotPassword;
